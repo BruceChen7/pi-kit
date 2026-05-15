9e001c8a673437810c2cf2dfbdb38245fefee824
bofengqiye <bofengqiye7@gmail.com>
2026-05-15 17:05:07 +0800

    chore: remove diffx review extension

--- extensions/diffx-review/index.ts
+++
@@ -1,9 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-import { registerDiffxReviewCommands } from "./commands.ts";
-import { registerDiffxReviewTools } from "./tools.ts";
-
-export default function diffxReviewExtension(pi: ExtensionAPI) {
-  registerDiffxReviewCommands(pi);
-  registerDiffxReviewTools(pi);
-}
